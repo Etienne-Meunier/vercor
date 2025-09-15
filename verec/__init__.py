01a78df158eb22f9f3e8147ea1e18f69614aa559
Roman Nuterman <nuterman@nbi.ku.dk>
2025-09-15 16:23:40 +0200

    Add versioneer and change build system

--- verec/__init__.py
+++ verec/__init__.py
@@ -5,3 +5,6 @@ from .fields import Field
 from .exchange import Exchange
 
 __all__ = ["Coupler", "Clock", "RectilinearGrid", "Field", "Exchange"]
+
+from . import _version
+__version__ = _version.get_versions()['version']
