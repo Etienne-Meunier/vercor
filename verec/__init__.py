b45695cf1ceb5b811a4f7977a05cd8e3b0c6f895
Roman Nuterman <nuterman@nbi.ku.dk>
2025-09-16 11:13:19 +0200

    Refactor code structure and remove redundant sections for improved readability and maintainability

--- verec/__init__.py
+++ verec/__init__.py
@@ -5,6 +5,3 @@ from verec.fields import Field
 from verec.exchange import Exchange
 
 __all__ = ["Coupler", "Clock", "RectilinearGrid", "Field", "Exchange"]
-
-from . import _version
-__version__ = _version.get_versions()['version']
