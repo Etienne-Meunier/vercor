ac8abd32266acb060492d0567a5f7ed85ee5d520
Roman Nuterman <nuterman@nbi.ku.dk>
2025-11-08 17:44:23 +0100

    Refactor test for BilinearRectilinear to remove unnecessary parameters and improve clarity

--- tests/test_regridders.py
+++ tests/test_regridders.py
@@ -4,7 +4,7 @@ from vercor.regridders.bilinear import BilinearRectilinear
 
 def test_bilinear_rectilinear(atm_grid, ocn_grid):
     # Bilinear regridder
-    regridder = BilinearRectilinear(atm_grid, None, ocn_grid, None)
+    regridder = BilinearRectilinear(atm_grid, ocn_grid)
     assert regridder.src_grid == atm_grid
     assert regridder.dst_grid == ocn_grid
     assert getattr(regridder, 'regridder') == None
