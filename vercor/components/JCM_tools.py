936a0f0e4585c3f8049adcd369230ee933c839a3
Tien-Yiao Hsu <tienyiao@pik-potsdam.de>
2025-11-18 13:37:47 +0100

    Linting, and follow the new initialization design

--- vercor/components/JCM_tools.py
+++ vercor/components/JCM_tools.py
@@ -1,21 +1,20 @@
-
-from typing import Dict, Tuple, Any, List
+from typing import Any, List
 import jax
 import jax.numpy as jnp
-from jax import tree_util
 
 
 def positive_cosine_cubic_latitude_squared(
     lat,
     amplitude: float = 1.0,
-) -> jnp.array:
-    return jnp.where(jnp.abs(lat) < jnp.pi/3, amplitude * jnp.cos(3*lat/2)**2, 0)
-
+) -> jnp.ndarray:
+    return jnp.where(
+        jnp.abs(lat) < jnp.pi / 3, amplitude * jnp.cos(3 * lat / 2) ** 2, 0
+    )
 
 
 def mean_leaf(
-    tree : any,
-    axis : int | list,
+    tree: Any,
+    axis: int | List[int],
 ):
     """
     A tool function that does the jnp.mean to leaf nodes.
@@ -27,13 +26,14 @@ def mean_leaf(
     Returns:
 
         tree_mean : tree with jnp.mean applied to each of its leaf node.
-        
+
     """
-    return jax.tree_util.tree_map(lambda arr: jnp.mean(arr,axis=axis), tree)
+    return jax.tree_util.tree_map(lambda arr: jnp.mean(arr, axis=axis), tree)
+
 
 def unwrap_leading_dims(
     obj,
-    first_n_dim = 2,
+    first_n_dim=2,
 ):
     """
     A tool function that unwraps the leading dimensions of jax arrays
@@ -45,8 +45,9 @@ def unwrap_leading_dims(
     Returns:
 
         unwrapped object.
-        
+
     """
+
     def _unwrap(arr):
         new_shape = (-1,) + arr.shape[first_n_dim:]
         return jnp.reshape(arr, new_shape)
@@ -55,7 +56,7 @@ def unwrap_leading_dims(
 
 
 def stack_objects(
-    objs : List,
+    objs: List,
 ):
     """
     A tool function that stack dataclasses together.
@@ -67,15 +68,16 @@ def stack_objects(
     Returns:
 
         stacked : Stacked object.
-        
+
     """
     # objs is a list of pytrees with same structure
     stacked = jax.tree_util.tree_map(lambda *xs: jnp.stack(xs), *objs)
     return stacked
 
+
 def concat_objects(
-    objs : List,
-    axis : int,
+    objs: List,
+    axis: int,
 ):
     """
     A tool function that concats dataclasses together.
@@ -87,11 +89,10 @@ def concat_objects(
     Returns:
 
         concatenated : Concatenated object.
-        
+
     """
     # objs is a list of pytrees with same structure
-    concatenated = jax.tree_util.tree_map(lambda *xs: jnp.concatenate(xs, axis=axis), *objs)
+    concatenated = jax.tree_util.tree_map(
+        lambda *xs: jnp.concatenate(xs, axis=axis), *objs
+    )
     return concatenated
-
-
-
