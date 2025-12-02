3ea3e307d4f6341fccf2bb9786ba5989c880cdb0
Tien-Yiao Hsu <tienyiao@pik-potsdam.de>
2025-12-02 16:42:53 +0100

    Update JCM to the most updated jax-gcm

--- vercor/components/JCM.py
+++ vercor/components/JCM.py
@@ -13,8 +13,10 @@ import jax_datetime as jdt
 from dinosaur import primitive_equations, primitive_equations_states
 
 import tree_math
+import jcm
 from jcm.model import Model
 from jcm.forcing import ForcingData, default_forcing
+from jcm.physics.speedy.physics_data import PhysicsData
 from jcm.physics_interface import dynamics_state_to_physics_state
 from jcm.physics_interface import PhysicsState
 from vercor.components.JCM_tools import mean_leaf, stack_objects, unwrap_leading_dims
@@ -50,7 +52,6 @@ class JCMState:
     phydata: Any
     metadata: primitive_equations_states
 
-
 class JCM(Component):
     """JCM Wrapper"""
 
@@ -77,51 +78,7 @@ class JCM(Component):
             binary_mask=np.where(model.geometry.fmask > 0.0, 1.0, 0.0).transpose() == 0.0,  # true = valid points.
             fraction_mask=model.geometry.fmask.transpose(),
         )
-
         super().__init__(name, grid)
-
-    def _initialize(
-        self,
-        initial_state: Optional[PhysicsState | primitive_equations.State] = None,
-        forcing: ForcingData = None,
-        start_date: jdt.Datetime = jdt.to_datetime("2000-01-01"),
-    ) -> JCMState:
-        model = self.model
-
-        # Copy from jax-gcm jcm/model.py
-        if isinstance(initial_state, primitive_equations.State):
-            model.initial_state = dynamics_state_to_physics_state(
-                initial_state, model.primitive
-            )
-            model._final_modal_state = initial_state
-        else:
-            model.initial_state = initial_state
-            model._final_modal_state = model._prepare_initial_modal_state(initial_state)
-
-            if initial_state is None:
-                model.initial_state = dynamics_state_to_physics_state(
-                    model._final_modal_state, model.primitive
-                )
-
-        model.start_date = start_date
-        model.forcing = forcing or default_forcing(self.model.coords.horizontal)
-
-        # The following code is a solution to have an initial value for phydata by stepping the model one time.
-        # The returned phydata is then used for the initial value.
-        _, init_phydata = self.model.physics.compute_tendencies(
-            state=model.initial_state,
-            forcing=model.forcing,
-            geometry=model.geometry,
-            date=model._date_from_sim_time(
-                jnp.array(model._final_modal_state.sim_time)
-            ),
-        )
-
-        return JCMState(
-            prog=asfloat64(model.initial_state),
-            phydata=asfloat64(init_phydata),
-            metadata=model._final_modal_state,
-        )
 
     def _generate_step_function(self, jitted: bool = True):
         def step_function(state, forcing, t):
@@ -143,14 +100,19 @@ class JCM(Component):
         return jax.jit(step_function) if jitted else step_function
 
     def initialize(self, coupler: "Coupler") -> None:
-        self._state = self._initialize()
+        _modal_state = self.model._prepare_initial_modal_state()
+        self._state = JCMState(
+            metadata = _modal_state,
+            phydata = PhysicsData.zeros(self.model.coords.horizontal.nodal_shape, self.model.coords.vertical.layers),
+            prog = dynamics_state_to_physics_state(_modal_state, self.model.primitive),
+        )
+        self.forcing = default_forcing(self.model.coords.horizontal)
         self._step_function = self._generate_step_function(jitted=self.jitted)
-
+        
         clock_start = coupler.clock.start
         grid_shape = self.grid.shape
 
         zeros = np.zeros(grid_shape)
-
         self.incoming_fields.SST = TNA(zeros + 273.15 + 15.0, clock_start, self.name)
         self.outgoing_fields.bottom_level_density = TNA(zeros + 1.22, clock_start, self.name)
         self.outgoing_fields.bottom_level_zonal_velocity = TNA(zeros, clock_start, self.name)
@@ -174,7 +136,7 @@ class JCM(Component):
                 f"dt={str(dt)} must be an integer multiple of coupling_timestep={str(self.coupling_timestep)}."
             )
 
-        _forcing = self.model.forcing.copy(
+        _forcing = self.forcing.copy(
             sea_surface_temperature=jnp.asarray(self.incoming_fields.SST).transpose(),
         )
 
@@ -197,19 +159,19 @@ class JCM(Component):
         d = _avg_predictions.dynamics
 
         # All the heat and freshwater fluxes are positive upward
-        self.outgoing_fields.net_surface_shortwave_radiation_flux = (- np.array(p.shortwave_rad.rsns).sum(axis=2).transpose(), time, self.name)
-        self.outgoing_fields.net_surface_longwave_radiation_flux = (np.array(p.surface_flux.rlns).sum(axis=2).transpose(), time, self.name)
-        self.outgoing_fields.bottom_level_zonal_velocity = (np.array(d.u_wind[:, :, -1]).transpose(), time, self.name)
-        self.outgoing_fields.bottom_level_meridional_velocity = (np.array(d.v_wind[:, :, -1]).transpose(), time, self.name)
-        self.outgoing_fields.bottom_level_potential_temperature = (np.array(d.temperature[:, :, -1]).transpose(), time, self.name)
-        self.outgoing_fields.bottom_level_temperature = (np.array(d.temperature[:, :, -1]).transpose(), time, self.name)
-        self.outgoing_fields.bottom_level_specific_humidity = (np.array(d.specific_humidity[:, :, -1]).transpose(), time, self.name)
-        self.outgoing_fields.bottom_level_height = (np.array(d.geopotential[:, :, -1] / jcm.constants.grav).transpose(), time, self.name)
+        self.outgoing_fields.net_surface_shortwave_radiation_flux = (- np.array(p.shortwave_rad.rsns).transpose(), time, self.name)
+        self.outgoing_fields.net_surface_longwave_radiation_flux = (np.array(p.surface_flux.rlns).transpose(), time, self.name)
+        self.outgoing_fields.bottom_level_zonal_velocity = (np.array(d.u_wind[:, :]).transpose(), time, self.name)
+        self.outgoing_fields.bottom_level_meridional_velocity = (np.array(d.v_wind[:, :]).transpose(), time, self.name)
+        self.outgoing_fields.bottom_level_potential_temperature = (np.array(d.temperature[:, :]).transpose(), time, self.name)
+        self.outgoing_fields.bottom_level_temperature = (np.array(d.temperature[:, :]).transpose(), time, self.name)
+        self.outgoing_fields.bottom_level_specific_humidity = (np.array(d.specific_humidity[:, :]).transpose(), time, self.name)
+        self.outgoing_fields.bottom_level_height = (np.array(d.geopotential[:, :] / jcm.constants.grav).transpose(), time, self.name)
         self.outgoing_fields.precipitation = ((
-            -np.array(p.condensation.precls + p.convection.precnv).transpose() / 1e3
+            - np.array(p.condensation.precls + p.convection.precnv).transpose() / 1e3
         ), time, self.name)
         self.outgoing_fields.evaporation = ((
-            np.array(p.surface_flux.evap / 1e3).sum(axis=2).transpose()
+            np.array(p.surface_flux.evap / 1e3).transpose()
         ), time, self.name)
 
     def _finalize(self, output: Optional[str] = None) -> xr.Dataset:
