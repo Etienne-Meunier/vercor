a1ded9f5ca0e612e7a5bebd13c9c44bbff0c9f92
Roman Nuterman <nuterman@nbi.ku.dk>
2026-01-06 13:56:33 +0100

    Refactor JCM forcing file generation and improve data directory handling; update run sequence order

--- vercor/components/external/jax_gcm_tools.py
+++ vercor/components/external/jax_gcm_tools.py
@@ -10,51 +10,57 @@ import jax.numpy as jnp
 
 
 def generate_jcm_forcing_and_topography_files(
-    resolution: int = 31,
-    data_directory: Optional[str | Path] = None,
+    resolution: int,
+    data_directory: Optional[Path] = None,
 ) -> Dict[str, Path]:
+
     import jcm
 
+    if not (isinstance(data_directory, Path) or data_directory is None):
+        raise TypeError("`data_directory` must be of type `Path` or `None`.")
 
     if data_directory is None:
-        print("Warning: `data_directory` is `None`. Attempting to set it to $HOME/.cache/jcm")
-        data_directory = os.environ.get('HOME', None)
-        if data_directory is None:
-            print("Warning: Cannot find environment variable $HOME. Use Path.cwd() instead.")
+        home_data_directory = os.environ.get("HOME", None)
+
+        if home_data_directory is None:
             data_directory = Path.cwd()
         else:
-            data_directory = Path(data_directory)
+            data_directory = Path(home_data_directory)
 
         data_directory = data_directory / ".cache/jcm"
-        print(f"Notice: Using `data_directory = \"{str(data_directory)}\".")
+
+        print(f"Using input data directory: \"{str(data_directory)}\".")
 
     raw_data_directory = Path(jcm.__file__).parent / f"data/bc"
+
     # Prepare boundary file
     files_to_check = dict(
-        terrain=(
-            data_directory / f"terrain_t{resolution:d}.nc"
-        ).resolve(),
-        forcing=(
-            data_directory / f"forcing_t{resolution:d}.nc"
-        ).resolve(),
+        terrain=(data_directory / f"terrain_t{resolution:d}.nc").resolve(),
+        forcing=(data_directory / f"forcing_t{resolution:d}.nc").resolve(),
     )
 
-    def check_if_file_exist(file_dict, verbose=True):
-        file_status = { file : Path(file).exists() for _, file in file_dict.items() }
+    def check_if_file_exist(
+        file_dict: Dict[str, Path], verbose: bool = True
+    ) -> Dict[Path, bool]:
+
+        file_status = {file: Path(file).exists() for _, file in file_dict.items()}
+
         if verbose:
             for file, result in file_status.items():
-                print(f"Check file {str(file):s}...", "found." if result else "not found.") 
+                print(
+                    f"Check file: {str(file):s}...", "found." if result else "not found."
+                )
+
         return file_status
 
     file_status = check_if_file_exist(files_to_check)
+
     if not all(list(file_status.values())):
-        print("Some files do not exist. Need to produce it.")
+        print("Some files are missing. Need to generate them.")
+
         try:
-            
             data_directory.mkdir(parents=True, exist_ok=True)
-            interpolation_code = (
-                raw_data_directory / "interpolate.py"
-            ).resolve()
+            interpolation_code = (raw_data_directory / "interpolate.py").resolve()
 
             result = subprocess.run(
                 [sys.executable, str(interpolation_code), f"{resolution:d}"],
@@ -63,24 +69,23 @@ def generate_jcm_forcing_and_topography_files(
                 text=True,
                 cwd=data_directory,
             )
-            
+
             for destination_file in files_to_check.values():
                 source_file = Path(raw_data_directory / destination_file.name)
-                print(f"Copying {str(source_file):s} => {str(destination_file):s}")
+                print(f"Copying: {str(source_file):s} => {str(destination_file):s}")
                 shutil.copy(source_file, destination_file)
 
         except subprocess.CalledProcessError as e:
             print("Error output:", e.stderr)
-    
+
         new_file_status = check_if_file_exist(files_to_check)
-        if all(list(new_file_status.values())):
-            print("All files exist!")
-        else:
+        if not all(list(new_file_status.values())):
             raise FileNotFoundError(
                 "Something went wrong. The daily file is not generated. Please check."
             )
 
     return files_to_check
+
 
 def mean_leaf(
     tree: Any,
