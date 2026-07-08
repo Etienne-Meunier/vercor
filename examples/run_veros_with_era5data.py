94d332861082b1bb01a99b92a26ef8869c980b2d
Roman Nuterman <nuterman@nbi.ku.dk>
2026-07-08 12:08:44 +0200

    Refactor make_veros_gcm call to include output frequency and variables

--- examples/run_veros_with_era5data.py
+++ examples/run_veros_with_era5data.py
@@ -14,7 +14,20 @@ from vercor.regridding import bilinear
 
 if __name__ == "__main__":
     atm = make_era5_atmosphere()
-    ocn = make_veros_gcm(restore_to_climatology=True)
+    ocn = make_veros_gcm(
+        restore_to_climatology=True,
+        output_frequency="month",
+        output_variables=(
+            "temp",
+            "salt",
+            "u",
+            "v",
+            "w",
+            "surface_taux",
+            "surface_tauy",
+            "psi",
+        )
+    )
     lnd = make_era5_land()
 
     # Clock and sequence
